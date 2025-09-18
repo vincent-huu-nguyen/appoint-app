6bcd388e531eecdb5dfabac59b32b9c874414b91
vinsanite <vincentnguyen7011@gmail.com>
2025-09-18 17:09:15 -0500

    testing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,7 @@ const Home = () => {
           Login
         </Link>
         <Link to="/register" className="bg-indigo-600 text-white px-6 py-2 rounded hover:bg-indigo-700 transition">
-          Register
+          Registe
         </Link>
       </div>
     </div>
